f034538cf0b606bac463c0adb1237e68553de356
VanBiak <s281867@ocsb.local>
2017-09-19 14:38:33 -0400

    Story Time

---
+++ src/Story.java
@@ -0,0 +1,12 @@
+
+public class Story {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("\tOnce upon a time, there was a man who wandered in the forest. One day, he saw a fruit at the top of the tree, and wanted to eat it.");
+		System.out.println("He saw a squirrel climb up the tree head first, and climb down head first. He was amazed by this, so he decided to try it out.");
+		System.out.println("He climbed up the tree head first and got the fruit, but on the way down he fell, and passed out.");
+		System.out.println("When he woke up, he made a big crater in the ground, and he thought an elephant almost stepped on him. ");
+	}
+
+}
